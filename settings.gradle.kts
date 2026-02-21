c4dd124ab48aaf3060160d043ec0358c7493a5aa
Elnaz <elnaz.hashemzade@gmail.com>
2026-02-21 12:14:09 +0330

    add separate cash

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
         maven { setUrl("https://maven.aliyun.com/repository/google") }
         maven { setUrl("https://maven.aliyun.com/repository/public") }
         maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-        google ()
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 plugins {
@@ -14,10 +14,10 @@ plugins {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
     }
 }
 
